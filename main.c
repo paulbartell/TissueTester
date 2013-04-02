08e0300297376d960403b1b95b9ba39a0916ccb5
Paul Bartell <paul.bartell@gmail.com>
2013-04-02 11:55:41 -0700

    added uart stuff

--- main.c
+++ main.c
@@ -6,8 +6,12 @@
  *
  *
  */
-#include "structs.h"
-#include "main.h"
+
+#define PART_LM4F120H5QR
+#define UART_BUFFERED
+
+//#include "datastructs.h"
+//#include "main.h"
 #include "driverlib/pin_map.h"
 #include "inc/hw_types.h"
 #include "inc/hw_memmap.h"
@@ -16,14 +20,19 @@
 #include "driverlib/gpio.h"
 #include "driverlib/timer.h"
 #include "driverlib/pin_map.h"
+#include "driverlib/uart.h"
+#include "utils/uartstdio.h"
+
 
 unsigned long dutyCycle, pwmPeriod;
 
-void main(void) {
 
-    // 40 MHz system clock
-    SysCtlClockSet(SYSCTL_SYSDIV_5|SYSCTL_USE_PLL|
-    SYSCTL_XTAL_16MHZ|SYSCTL_OSC_MAIN);
+
+
+void loop() {
+
+
+
 }
 // Sets up our timer for PWM output
 void pwmSetup() {
@@ -32,10 +41,15 @@ void pwmSetup() {
 	TimerConfigure(TIMER0_BASE, TIMER_CFG_SPLIT_PAIR|TIMER_CFG_A_PWM); // set as pwm type
 
 	dutyCycle = 250; // 75 % duty cycle (0.25 * ulPeriod)
+	pwmPeriod = 1000;
 
 	TimerLoadSet(TIMER0_BASE, TIMER_A, pwmPeriod - 1);
-	// TimerMatchSet(TIMER0_BASE, TIMER_A, pwmPeriod / 2);
+	TimerMatchSet(TIMER0_BASE, TIMER_A, 0); // set duty cycle to 0
 	TimerEnable(TIMER0_BASE, TIMER_A);
+    SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOF);
+    GPIOPinTypeGPIOOutput(GPIO_PORTF_BASE, GPIO_PIN_1|GPIO_PIN_2|GPIO_PIN_3);
+    GPIOPinWrite(GPIO_PORTF_BASE, GPIO_PIN_1|GPIO_PIN_2|GPIO_PIN_3, 5);
+
 }
 
 // sets our duty cycle
@@ -43,3 +57,40 @@ void pwmSetDuty(unsigned long duty) {
 	dutyCycle = pwmPeriod - duty;
 	TimerMatchSet(TIMER0_BASE, TIMER_A, duty);
 }
+
+void updatePID() {
+
+}
+
+void setPosition(){
+
+}
+
+void main(void) {
+
+    // 80 MHz system clock
+    SysCtlClockSet(SYSCTL_SYSDIV_2_5 | SYSCTL_USE_PLL | SYSCTL_OSC_MAIN | SYSCTL_XTAL_16MHZ);
+
+
+    // Stuff for PWM initialization
+    SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOB);
+    GPIOPinConfigure(GPIO_PB6_T0CCP0);
+    GPIOPinTypeTimer(GPIO_PORTB_BASE, GPIO_PIN_6);
+
+    pwmSetup();
+    pwmSetDuty(250);
+
+
+    // stuff for UART initialization
+    SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOA);
+    GPIOPinConfigure(GPIO_PA0_U0RX);
+    GPIOPinConfigure(GPIO_PA1_U0TX);
+    GPIOPinTypeUART(GPIO_PORTA_BASE, GPIO_PIN_0 | GPIO_PIN_1);
+    UARTStdioInit(0);
+    UARTprintf("Hello world!\n");
+
+
+    while(1){
+    }
+}
+
